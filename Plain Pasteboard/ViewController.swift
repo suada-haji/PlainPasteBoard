1bb6a36e45def91a40fbe026d8124304211ecd56
Suada Haji <suada.haji@andela.com>
2020-01-23 15:20:45 +0300

    Initial commit

---
+++ Plain Pasteboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plain Pasteboard
+//
+//  Created by Suada Haji on 23/01/2020.
+//  Copyright © 2020 Suada Haji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
